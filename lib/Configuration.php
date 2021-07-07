a99052a18ab76e82f42480fb13e8e85ba78a95ee
Jesse Evers <jesse@jesseevers.com>
2021-07-07 15:40:00 -0600

    Bump version to 3.0.3

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -62,7 +62,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'jlevers/selling-partner-api/3.0.2 (Language=PHP)';
+    protected $userAgent = 'jlevers/selling-partner-api/3.0.3 (Language=PHP)';
 
     /**
      * Debug switch (default set to false)
@@ -299,7 +299,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 2020-11-01' . PHP_EOL;
-        $report .= '    SDK Package Version: 3.0.2' . PHP_EOL;
+        $report .= '    SDK Package Version: 3.0.3' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . $tempFolderPath . PHP_EOL;
 
         return $report;

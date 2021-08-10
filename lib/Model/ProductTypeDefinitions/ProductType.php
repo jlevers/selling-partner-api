606624273c63757b0840fdda2ae0c4bf59f47dee
Jesse Evers <jesse@jesseevers.com>
2021-08-09 22:06:44 -0400

    Update Product Type Definitions API

--- lib/Model/ProductTypeDefinitions/ProductType.php
+++ lib/Model/ProductTypeDefinitions/ProductType.php
@@ -11,7 +11,7 @@
 /**
  * Selling Partner API for Product Type Definitions
  *
- * The Selling Partner API for Product Type Definitions provides programmatic access to attribute and data requirements for product types in the Amazon catalog. Use this API to return the JSON Schema for a product type that you can then use with other Selling Partner APIs, such as the Selling Partner API for Listings Items, the Selling Partner API for Catalog Items, and the Selling Partner API for Feeds (for JSON-based listing feeds).
+ * The Selling Partner API for Product Type Definitions provides programmatic access to attribute and data requirements for product types in the Amazon catalog. Use this API to return the JSON Schema for a product type that you can then use with other Selling Partner APIs, such as the Selling Partner API for Listings Items, the Selling Partner API for Catalog Items, and the Selling Partner API for Feeds (for JSON-based listing feeds). For more information, see the [Product Type Definitions API Use Case Guide](https://github.com/amzn/selling-partner-api-docs/blob/main/guides/en-US/use-case-guides/product-type-definitions-api-use-case-guide/definitions-product-types-api-use-case-guide_2020-09-01.md).
  *
  * The version of the OpenAPI document: 2020-09-01
  * 

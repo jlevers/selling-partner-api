e361ca5628325be408696e8eaa4f092d590eda9b
Jesse Evers <jesse@jesseevers.com>
2021-10-17 13:41:57 -0400

    Regenerate OrderItem with new deemed reseller categories

--- lib/Model/Orders/OrderItem.php
+++ lib/Model/Orders/OrderItem.php
@@ -327,6 +327,8 @@ class OrderItem implements ModelInterface, ArrayAccess, \JsonSerializable
     const DEEMED_RESELLER_CATEGORY_IOSS = 'IOSS';
     const DEEMED_RESELLER_CATEGORY_UOSS = 'UOSS';
     const DEEMED_RESELLER_CATEGORY_GB_VOEC = 'GB_VOEC';
+    const DEEMED_RESELLER_CATEGORY_NO_VOEC = 'NO_VOEC';
+    const DEEMED_RESELLER_CATEGORY_CA_MPF = 'CA_MPF';
     
 
     
@@ -341,6 +343,8 @@ class OrderItem implements ModelInterface, ArrayAccess, \JsonSerializable
             self::DEEMED_RESELLER_CATEGORY_IOSS,
             self::DEEMED_RESELLER_CATEGORY_UOSS,
             self::DEEMED_RESELLER_CATEGORY_GB_VOEC,
+            self::DEEMED_RESELLER_CATEGORY_NO_VOEC,
+            self::DEEMED_RESELLER_CATEGORY_CA_MPF,
         ];
     }
     

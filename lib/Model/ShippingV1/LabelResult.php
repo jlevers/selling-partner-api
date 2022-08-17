5a445109c7f4cbe4782038785a367becaf1ee172
Jesse Evers <jesse@jesseevers.com>
2022-08-17 18:12:10 -0400

    Update ShippingV1 API documentation

--- lib/Model/ShippingV1/LabelResult.php
+++ lib/Model/ShippingV1/LabelResult.php
@@ -11,7 +11,7 @@
 /**
  * Selling Partner API for Shipping
  *
- * Provides programmatic access to Amazon Shipping APIs.
+ * Provides programmatic access to Amazon Shipping APIs.  **Note:** If you are new to the Amazon Shipping API, refer to the latest version of <a href=\"https://developer-docs.amazon.com/amazon-shipping/docs/shipping-api-v2-reference\">Amazon Shipping API (v2)</a> on the <a href=\"https://developer-docs.amazon.com/amazon-shipping/\">Amazon Shipping Developer Documentation</a> site.
  *
  * The version of the OpenAPI document: v1
  * 

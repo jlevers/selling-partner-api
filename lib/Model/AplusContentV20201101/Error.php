4de2f682814ca48270eb2fd904cfedcef9eab19b
Jesse Evers <jesse@jesseevers.com>
2023-05-16 13:37:15 -0400

    Remove setMessage length validation on APlusContent Errors

--- lib/Model/AplusContentV20201101/Error.php
+++ lib/Model/AplusContentV20201101/Error.php
@@ -153,10 +153,6 @@ class Error extends BaseModel implements ModelInterface, ArrayAccess, \JsonSeria
         if ($this->container['message'] === null) {
             $invalidProperties[] = "'message' can't be null";
         }
-        if ((mb_strlen($this->container['message']) < 1)) {
-            $invalidProperties[] = "invalid value for 'message', the character length must be bigger than or equal to 1.";
-        }
-
         return $invalidProperties;
     }
 

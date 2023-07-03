7737702970f627ad42a6969a7adf8e3f5ba43c8d
Jesse Evers <jesse@jesseevers.com>
2023-07-03 13:41:18 -0400

    Update Order models

--- lib/Model/OrdersV0/BuyerRequestedCancel.php
+++ lib/Model/OrdersV0/BuyerRequestedCancel.php
@@ -11,7 +11,7 @@
 /**
  * Selling Partner API for Orders
  *
- * The Selling Partner API for Orders helps you programmatically retrieve order information. These APIs let you develop fast, flexible, custom applications in areas like order synchronization, order research, and demand-based decision support tools.
+ * The Selling Partner API for Orders helps you programmatically retrieve order information. These APIs let you develop fast, flexible, custom applications in areas like order synchronization, order research, and demand-based decision support tools. The Orders API only supports orders that are less than two years old. Orders more than two years old will not show in the API response.
  *
  * The version of the OpenAPI document: v0
  * 
@@ -29,7 +29,6 @@ namespace SellingPartnerApi\Model\OrdersV0;
 use ArrayAccess;
 use SellingPartnerApi\Model\BaseModel;
 use SellingPartnerApi\Model\ModelInterface;
-use SellingPartnerApi\ObjectSerializer;
 
 /**
  * BuyerRequestedCancel Class Doc Comment
--- lib/Model/OrdersV0/Order.php
+++ lib/Model/OrdersV0/Order.php
@@ -29,7 +29,6 @@ namespace SellingPartnerApi\Model\OrdersV0;
 use ArrayAccess;
 use SellingPartnerApi\Model\BaseModel;
 use SellingPartnerApi\Model\ModelInterface;
-use SellingPartnerApi\ObjectSerializer;
 
 /**
  * Order Class Doc Comment

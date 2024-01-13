a44e920c42014395c3fd77f0f1989248308737bd
Alex Otchenashev <alexandr.otchenashev@gmail.com>
2024-01-13 19:07:38 +0100

    Add support for NG_VOEC Deemed Reseller Category

--- lib/Model/OrdersV0/OrderItem.php
+++ lib/Model/OrdersV0/OrderItem.php
@@ -282,6 +282,7 @@ class OrderItem extends BaseModel implements ModelInterface, ArrayAccess, \JsonS
     const DEEMED_RESELLER_CATEGORY_CA_MPF = 'CA_MPF';
     const DEEMED_RESELLER_CATEGORY_AU_VOEC = 'AU_VOEC';
     const DEEMED_RESELLER_CATEGORY_NZ_VOEC = 'NZ_VOEC';
+    const DEEMED_RESELLER_CATEGORY_NG_VOEC = 'NG_VOEC';
     const DEEMED_RESELLER_CATEGORY_JE_VOEC = 'JE_VOEC';
     const DEEMED_RESELLER_CATEGORY_CH_SUPPLIER_IMPORT = 'CH_SUPPLIER_IMPORT';
     
@@ -303,6 +304,7 @@ class OrderItem extends BaseModel implements ModelInterface, ArrayAccess, \JsonS
             self::DEEMED_RESELLER_CATEGORY_CA_MPF,
             self::DEEMED_RESELLER_CATEGORY_AU_VOEC,
             self::DEEMED_RESELLER_CATEGORY_NZ_VOEC,
+            self::DEEMED_RESELLER_CATEGORY_NG_VOEC,
             self::DEEMED_RESELLER_CATEGORY_JE_VOEC,
             self::DEEMED_RESELLER_CATEGORY_CH_SUPPLIER_IMPORT,
         ];

5d440945368528d235ddf49c144af2d67adf5bfb
Jesse Evers <jesse@jesseevers.com>
2024-03-21 12:58:59 -0400

    Move EnumTrait to src/Traits/

--- src/Enums/ApiCategory.php
+++ src/Enums/ApiCategory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace SellingPartnerApi\Enums;
 
+use SellingPartnerApi\Traits\EnumTrait;
+
 enum ApiCategory: string
 {
     use EnumTrait;
--- src/Traits/EnumTrait.php
+++ src/Traits/EnumTrait.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SellingPartnerApi\Enums;
+namespace SellingPartnerApi\Traits;
 
 trait EnumTrait
 {

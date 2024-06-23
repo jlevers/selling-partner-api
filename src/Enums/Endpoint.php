557b37a4bfed1a004d3d1ffc4efd8ac2396026ec
Jesse Evers <jesse@jesseevers.com>
2024-06-23 11:41:01 -0400

    Add Endpoint::byCountryCode() and Endpoint::byRegion()

--- src/Enums/Endpoint.php
+++ src/Enums/Endpoint.php
@@ -51,4 +51,52 @@ enum Endpoint: string
             default => throw new InvalidArgumentException("Unknown marketplace ID $marketplaceId"),
         };
     }
+
+    public static function byCountryCode(string $countryCode, bool $sandbox = false): self
+    {
+        $countryToMarketplaceId = [
+            'BR' => 'A2Q3Y263D00KWC',
+            'CA' => 'A2EUQ1WTGCTBG2',
+            'MX' => 'A1AM78C64UM0Y8',
+            'US' => 'ATVPDKIKX0DER',
+
+            'AE' => 'A2VIGQ35RCS4UG',
+            'BE' => 'AMEN7PMS3EDWL',
+            'DE' => 'A1PA6795UKMFR9',
+            'EG' => 'ARBP9OOSHTCHU',
+            'ES' => 'A1RKKUPIHCS9HS',
+            'FR' => 'A13V1IB3VIYZZH',
+            // UK and GB are both common country codes for the United Kingdom
+            'GB' => 'A1F83G8C2ARO7P',
+            'UK' => 'A1F83G8C2ARO7P',
+            'IN' => 'A21TJRUUN4KGV',
+            'IT' => 'APJ6JRA9NG5V4',
+            'NL' => 'A1805IZSGTT6HS',
+            'PL' => 'A1C3SOZRARQ6R3',
+            'SA' => 'A17E79C6D8DWNP',
+            'SE' => 'A2NODRKZP88ZB9',
+            'TR' => 'A33AVAJ2PDY3EV',
+            'ZA' => 'AE08WJ6YKNBMC',
+
+            'AU' => 'A39IBJ37TRP1C6',
+            'JP' => 'A1VC38T7YXB528',
+            'SG' => 'A19VAU5U5O7RUS',
+        ];
+
+        if (! isset($countryToMarketplaceId[$countryCode])) {
+            throw new InvalidArgumentException("Unknown country code $countryCode");
+        }
+
+        return self::byMarketplaceId($countryToMarketplaceId[$countryCode], $sandbox);
+    }
+
+    public static function byRegion(string $region, bool $sandbox = false): self
+    {
+        return match ($region) {
+            'NA' => $sandbox ? self::NA_SANDBOX : self::NA,
+            'EU' => $sandbox ? self::EU_SANDBOX : self::EU,
+            'FE' => $sandbox ? self::FE_SANDBOX : self::FE,
+            default => throw new InvalidArgumentException("Unknown region $region"),
+        };
+    }
 }

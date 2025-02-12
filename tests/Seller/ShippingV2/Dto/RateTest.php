7fe8e1c6cdad6d41ef720608dcb182c516048b8c
jesse <jesse@jesseevers.com>
2025-02-12 07:06:55 -0500

    Add test for deserializing nullable DateTimeInterface attributes

---
+++ tests/Seller/ShippingV2/Dto/RateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SellingPartnerApi\Tests\Seller\ShippingV2\Dto;
+
+use PHPUnit\Framework\TestCase;
+use SellingPartnerApi\Seller\ShippingV2\Dto\Rate;
+
+class RateTest extends TestCase
+{
+    public function testDeserializeNullDateTime()
+    {
+        $result = Rate::deserialize([
+            'rateId' => '',
+            'carrierId' => '',
+            'carrierName' => '',
+            'serviceId' => '',
+            'serviceName' => '',
+            'totalCharge' => [
+                'value' => 4.88,
+                'unit' => 'USD'
+            ],
+            'promise' => [
+                'deliveryWindow' => [
+                    'start' => '2024-12-29T07:59:59Z',
+                    'end' => '2024-12-29T07:59:59Z'
+                ],
+                'pickupWindow' => [
+                    'start' => null,
+                    'end' => null
+                ]
+            ],
+            'supportedDocumentSpecifications' => [],
+            'requiresAdditionalInputs' => false,
+            'billedWeight' => [
+                'unit' => 'GRAM',
+                'value' => 205
+            ],
+            'availableValueAddedServiceGroups' => [],
+        ]);
+        $this->assertNotNull($result);
+        $this->assertNull($result->promise->pickupWindow->start);
+    }
+}

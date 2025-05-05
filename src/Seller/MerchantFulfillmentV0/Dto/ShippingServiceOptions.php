fb91e2744bfcabb8711fdd1d79d51e0e612c7b95
jesse <jesse@jesseevers.com>
2025-05-05 11:01:11 -0700

    Make CarrierWillPickUp nullable in ShippingServiceOptions (#874)

--- src/Seller/MerchantFulfillmentV0/Dto/ShippingServiceOptions.php
+++ src/Seller/MerchantFulfillmentV0/Dto/ShippingServiceOptions.php
@@ -16,23 +16,23 @@ final class ShippingServiceOptions extends Dto
 {
     protected static array $attributeMap = [
         'deliveryExperience' => 'DeliveryExperience',
-        'carrierWillPickUp' => 'CarrierWillPickUp',
         'declaredValue' => 'DeclaredValue',
+        'carrierWillPickUp' => 'CarrierWillPickUp',
         'carrierWillPickUpOption' => 'CarrierWillPickUpOption',
         'labelFormat' => 'LabelFormat',
     ];
 
     /**
      * @param  string  $deliveryExperience  The delivery confirmation level.
-     * @param  bool  $carrierWillPickUp  When true, the carrier will pick up the package. Note: Scheduled carrier pickup is available only using Dynamex (US), DPD (UK), and Royal Mail (UK).
      * @param  ?CurrencyAmount  $declaredValue  Currency type and amount.
+     * @param  ?bool  $carrierWillPickUp  When true, the carrier will pick up the package. Note: Scheduled carrier pickup is available only using Dynamex (US), DPD (UK), and Royal Mail (UK).
      * @param  ?string  $carrierWillPickUpOption  Carrier will pick up option.
      * @param  ?string  $labelFormat  The label format.
      */
     public function __construct(
         public string $deliveryExperience,
-        public bool $carrierWillPickUp,
         public ?CurrencyAmount $declaredValue = null,
+        public ?bool $carrierWillPickUp = null,
         public ?string $carrierWillPickUpOption = null,
         public ?string $labelFormat = null,
     ) {}

7cecd56b191cbb0710ec55479c77009d92e166a6
jesse <jesse@jesseevers.com>
2026-05-24 11:15:17 -0700

    Remove unnecessary modifications.json entry

--- src/Seller/OrdersV0/Dto/OrderBuyerInfo.php
+++ src/Seller/OrdersV0/Dto/OrderBuyerInfo.php
@@ -21,7 +21,6 @@ final class OrderBuyerInfo extends Dto
         'buyerCounty' => 'BuyerCounty',
         'buyerTaxInfo' => 'BuyerTaxInfo',
         'purchaseOrderNumber' => 'PurchaseOrderNumber',
-        'buyerEmail' => 'BuyerEmail',
     ];
 
     /**
@@ -33,7 +32,6 @@ final class OrderBuyerInfo extends Dto
      * **Note**: This attribute is only available in the Brazil marketplace.
      * @param  ?BuyerTaxInfo  $buyerTaxInfo  Tax information about the buyer.
      * @param  ?string  $purchaseOrderNumber  The purchase order (PO) number entered by the buyer at checkout. Only returned for orders where the buyer entered a PO number at checkout.
-     * @param  ?string  $buyerEmail  The anonymized email address of the buyer.
      */
     public function __construct(
         public string $amazonOrderId,
@@ -42,6 +40,5 @@ final class OrderBuyerInfo extends Dto
         public ?string $buyerCounty = null,
         public ?BuyerTaxInfo $buyerTaxInfo = null,
         public ?string $purchaseOrderNumber = null,
-        public ?string $buyerEmail = null,
     ) {}
 }
